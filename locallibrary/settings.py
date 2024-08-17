87b791fe0b12dd44deedbc7bd0a4558dea13d56f
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2024-08-17 23:10:10 +0000

    install django-extensions package

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Add our new application
     'catalog.apps.CatalogConfig', # This object was created for us in /catalog/apps.py
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
