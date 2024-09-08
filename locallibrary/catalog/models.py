85109e3351bf95f94bf384775b01eb917fc241cf
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2024-09-08 22:21:55 +0000

    add borrower foreign key & overdue method w property decorator to bookinstance and migrated

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse # Used in get_absolute_url() to get URL for spec
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
 import uuid # Required for unique book instances
+from django.conf import settings
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -66,6 +68,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -88,6 +91,11 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.book.title}, Status: {self.status}, Due back: {self.due_back}, ID: {self.id}'
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
 class Author(models.Model):
     """Model representing an author."""
