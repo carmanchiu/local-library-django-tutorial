41eecfcd87c934881c7e07dcc73301c1b01b8802
Carman Chiu <101212528+carmanchiu@users.noreply.github.com>
2024-12-17 01:26:29 +0000

    implemented the all borrowed books challenge

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -124,9 +124,22 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
             .filter(status__exact='o')
             .order_by('due_back')
         )
-    
-import datetime
+
+# Added as part of challenge!
 from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission."""
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+
+import datetime
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
